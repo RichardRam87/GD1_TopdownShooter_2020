826425f1dc38287a0ae4bdf05f1d9afdcc47dbfc
Richard Ram <richard@richard-ram.com>
2020-02-24 11:53:08 +0100

    Init commit, dvt les 2

---
+++ Assets/PlayerMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private CharacterController _characterController;
+
+    private void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float zInput = Input.GetAxisRaw("Vertical");
+        Vector3 inputDirection = new Vector3(xInput, 0f, zInput);
+        inputDirection.Normalize();
+              
+        _characterController.Move(inputDirection * _speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerRotator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotator : MonoBehaviour
+{
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 lookPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+            transform.LookAt(lookPosition);
+        }
+        Debug.DrawRay(ray.origin, ray.direction*100, Color.green);
+    }
+}
